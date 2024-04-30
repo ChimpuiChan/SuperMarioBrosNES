095f737042c157c3886209786d33f6567536a608
ChimpuiChan <chimpuichanloli@gmail.com>
2024-05-01 01:10:42 +0530

    Block hits and hidden blocks implemented
    Now Mario can hit blocks! It looks and feels so amazing. I liked the way Math was used to animate it. It just used the Vector3.Lerp() for both going up and coming down when Mario hit the block with his head.
    Block collision detection done using DotProd() defined in the Extensions script to check if Mario is moving in the upward direction to hit the block.
    Hidden blocks are a piece of cake, just disable the sprite renderer as it gets enabled each time the HitBlock() function is called during a collision event. However, Mario can stand on the invisible block even when it is not rendered. Hopefully that would be fixed soon.

---
+++ Super Mario Bros NES/Assets/BlockHit.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using UnityEngine;
+
+public class BlockHit : MonoBehaviour
+{
+    // -1 means the block can be hit infinite number of times
+    // It just comes down to the fact that we don't actually get to 0, so since we haven't specified anything to do when it is less than 0, it just simply does nothing
+    public int maxHits = -1;
+    private SpriteRenderer spriteRenderer;
+    public Sprite hitState;
+
+    // Need this boolean variable for making sure that Mario cannot hit the block while it's animating
+    private bool isAnimating;
+
+    // For animating block when hit
+    private Vector3 blockPosition;
+    private Vector3 upwardPosition;
+    private float elapsedTime;
+    private float animationDuration;
+    private float lerpTime;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isAnimating && maxHits != 0 && collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.transform.DotProd(transform, Vector2.up))
+            {
+                // If game object collided with is Mario and if he is colliding with his head, then he has hit the block provided that the block is not currently animating and it has maxHits atleast 1
+                // For maxHits = -1, the block can be hit infinite number of times
+                // collision.transform is Mario and transform is the transform of this block
+                // We are comparing the direction of Mario's hit to the block
+                HitBlock();
+            }
+        }
+    }
+
+    private void HitBlock()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // For hidden blocks to appear when hit
+        spriteRenderer.enabled = true;
+
+        maxHits--;
+
+        if (maxHits == 0)
+        {
+            spriteRenderer.sprite = hitState;
+        }
+
+        StartCoroutine(HitAnimation());
+    }
+
+    // For animating the block when hit using Math :P
+    private IEnumerator HitAnimation()
+    {
+        isAnimating = true;
+
+        // transform.localPosition is used to move this object relative to its parent
+        // Suppose in the world space, the parent is at coordinates (0, 0, 0) and this block that is attached to it is 1 unit above from world space, the world space coordinates of the block will be (1, 0, 0)
+        // However it's local space coordinates are relative to its parent's coordinates and hence its local position is at coordinates (0, 0, 0)
+        // This is done to ensure that if this object is attached to any parent object, it still moves relative to its parent while still being able to move independantly
+        blockPosition = transform.localPosition;
+        upwardPosition = blockPosition + Vector3.up * 0.5f;
+
+        // Move up
+        yield return MoveBlock(blockPosition, upwardPosition);
+
+        // Come down
+        yield return MoveBlock(upwardPosition, blockPosition);
+
+        isAnimating = false;
+    }
+
+    private IEnumerator MoveBlock(Vector3 currentPosition, Vector3 targetPosition)
+    {
+        elapsedTime = 0f;
+        animationDuration = 0.125f;
+
+        while (elapsedTime < animationDuration)
+        {
+            lerpTime = elapsedTime / animationDuration;
+
+            transform.localPosition = Vector3.Lerp(currentPosition, targetPosition, lerpTime);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        // Just to make sure it has reached the target position coz elapsed time may not always reach the exact value of animation duration
+        transform.localPosition = targetPosition;
+    }
+}
